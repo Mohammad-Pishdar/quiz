77c1397ab6bf009abcdc455f3b398cc73e541ef5
Mo Pishdar <mohpish@gmail.com>
2020-06-09 07:07:18 +1000

    We now have an array of questions and answers in our javascript file

--- script.js
+++ script.js
@@ -6,3 +6,69 @@ startButton.addEventListener("click", function () {
     startQuizContainer.classList.replace("show", "hide");
     questionsContainer.classList.replace("hide", "show");
 })
+
+const questions = [{
+        question: "Commonly used data types DO NOT include:",
+        answers: [{
+            text: "1. strings",
+            correct: false
+        }, {
+            text: "2. booleans",
+            correct: false
+        }, {
+            text: "3. alerts",
+            correct: true
+        }, {
+            text: "4. numbers",
+            correct: false
+        }]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____________.",
+        answers: [{
+            text: "1. numbers and strings",
+            correct: false
+        }, {
+            text: "2. other arrays",
+            correct: false
+        }, {
+            text: "3. booleans",
+            correct: false
+        }, {
+            text: "4. all of the above",
+            correct: true
+        }]
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within _________.",
+        answers: [{
+            text: "1. qoutes",
+            correct: false
+        }, {
+            text: "2. curly brackets",
+            correct: false
+        }, {
+            text: "3. parentheses",
+            correct: true
+        }, {
+            text: "4. square brakets",
+            correct: false
+        }]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [{
+            text: "1. script",
+            correct: true
+        }, {
+            text: "2. javascript",
+            correct: false
+        }, {
+            text: "3. js",
+            correct: false
+        }, {
+            text: "4. scripting",
+            correct: false
+        }]
+    }
+]
